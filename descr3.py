9c6d9df64e0314c2c4523f19a19f77d1a5a92f2a
Marshall T. Rose <mrose17@gmail.com>
2017-02-08 11:49:23 -0800

    Minor updates
    - add .gitignore so input/output files aren’t uploaded
    - add optional parameter to `BeautifulSoup` to avoid a warning during
    execution
    - s/out3.txt/output.txt/ to match `README.md`
    -

--- descr3.py
+++ descr3.py
@@ -18,7 +18,7 @@ for i in range(0, len(urls)):
 	try:
 		url= urls[i]
 		response = requests.get(url)
-		soup = BeautifulSoup(response.text)
+		soup = BeautifulSoup(response.text, "html.parser")
 		metas = soup.find_all('meta')
 		a=[ meta.attrs['content'] for meta in metas if 'name' in meta.attrs and meta.attrs['name'] == 'description' ]
 		# print a
